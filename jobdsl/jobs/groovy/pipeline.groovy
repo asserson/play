b66c909a5ec0e702be5c7b9cc529331187e06705
chas <asserson@gmail.com>
2017-06-12 18:38:34 +0200

    change pipline to run a different script

--- jobdsl/jobs/groovy/pipeline.groovy
+++ jobdsl/jobs/groovy/pipeline.groovy
@@ -5,5 +5,5 @@ import javaposse.jobdsl.dsl.DslFactory
 new JobBuilder(this as DslFactory, "jenkins_as_a_code-pipeline", "pipeline")
     .addLogRotator()
     .addScmPollTrigger("@midnight")
-    .addPipelineDefinitionFile("jobdsl/jobs/resources/pipelines/jenkinsdeploy.groovy")
+    .addPipelineDefinitionFile("jobdsl/jobs/resources/pipelines/run_a_script.groovy")
     .build()
